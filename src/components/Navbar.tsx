082dc6a991ccc2dfc2f2ccc754fd68904600797f
Nitish kumar <kumarn66346@gmail.com>
2026-09-14 21:36:11 +0530

    Fixed errors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import Link from 'next/link'
 import { Company } from './Company'
 import { Oursolutions } from './Oursolutions'
-import QRScanner from './QRScanner'
 
 export const Navbar = () => {
   return (
